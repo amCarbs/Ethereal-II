2770fd58938bd8c7cb4d0fc74d6f578b293e5a9c
Nick <ldisguisel@gmail.com>
2014-10-15 20:18:39 -0500

    Delete dropEditorCompiler.bat

--- dropEditorCompiler.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Compiling
-"C:/Program Files/Java/jdk1.7.0_65/bin/javac.exe" -d bin -cp lib/*; -sourcepath src src/com/rs/*.java
-"C:/Program Files/Java/jdk1.7.0_65/bin/javac.exe" -d bin -cp lib/*; -sourcepath src src/com/rs/game/npc/familiar/*.java
-"C:/Program Files/Java/jdk1.7.0_65/bin/javac.exe" -d bin -cp lib/*; -sourcepath src src/com/rs/tools/dropEditor.java
-echo Complete
-pause
